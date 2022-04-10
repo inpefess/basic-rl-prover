f57adb803fbaa40e5a8de008a81d24fb0bcc3945
Boris Shminke <boris@shminke.ml>
2022-04-10 16:07:47 +0200

    update DQNTrainer code and increase coverage

--- basic_rl_prover/custom_dqn_trainer.py
+++ basic_rl_prover/custom_dqn_trainer.py
@@ -19,7 +19,7 @@
 Customised DQN Trainer
 ======================
 """
-from typing import Optional, Type, Union
+from typing import Optional, Type
 
 from ray.rllib.agents.dqn.dqn import DQNTrainer, calculate_rr_weights
 from ray.rllib.evaluation.worker_set import WorkerSet
@@ -35,6 +35,7 @@ from ray.rllib.execution.train_ops import (
 from ray.rllib.policy.policy import Policy
 from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.utils.annotations import override
+from ray.rllib.utils.metrics.learner_info import LEARNER_STATS_KEY
 from ray.rllib.utils.typing import SampleBatchType, TrainerConfigDict
 from ray.util.iter import LocalIterator
 
@@ -78,23 +79,48 @@ class CustomDQNTrainer(DQNTrainer):
 
     @staticmethod
     @override(DQNTrainer)
+    # pylint: disable=too-many-statements
     def execution_plan(
         workers: WorkerSet, config: TrainerConfigDict, **kwargs
     ) -> LocalIterator[dict]:
         """this function is moslty a copy of the base class implementation"""
+        local_replay_buffer = kwargs["local_replay_buffer"]
         rollouts = ParallelRollouts(workers, mode="bulk_sync")
         store_op = rollouts.for_each(
-            CustomStoreToReplayBuffer(
-                local_buffer=kwargs["local_replay_buffer"]
-            )
+            CustomStoreToReplayBuffer(local_buffer=local_replay_buffer)
         )
+
+        # pylint: disable=too-many-statements
+        def update_prio(item):
+            samples, info_dict = item
+            if config.get("prioritized_replay"):
+                prio_dict = {}
+                for policy_id, info in info_dict.items():
+                    td_error = info.get(
+                        "td_error", info[LEARNER_STATS_KEY].get("td_error")
+                    )
+                    samples.policy_batches[policy_id].set_get_interceptor(None)
+                    batch_indices = samples.policy_batches[policy_id].get(
+                        "batch_indexes"
+                    )
+                    if len(batch_indices) != len(td_error):
+                        # pylint: disable=invalid-name
+                        T = local_replay_buffer.replay_sequence_length
+                        assert (
+                            len(batch_indices) > len(td_error)
+                            and len(batch_indices) % T == 0
+                        )
+                        batch_indices = batch_indices.reshape([-1, T])[:, 0]
+                        assert len(batch_indices) == len(td_error)
+                    prio_dict[policy_id] = (batch_indices, td_error)
+                local_replay_buffer.update_priorities(prio_dict)
+            return info_dict
+
         post_fn = config.get("before_learn_on_batch") or (lambda b, *a: b)
         if config["simple_optimizer"]:
-            train_step_op: Union[
-                TrainOneStep, MultiGPUTrainOneStep
-            ] = TrainOneStep(workers)
+            train_step_op = TrainOneStep(workers)
         else:
-            train_step_op = MultiGPUTrainOneStep(
+            train_step_op = MultiGPUTrainOneStep(  # type: ignore
                 workers=workers,
                 sgd_minibatch_size=config["train_batch_size"],
                 num_sgd_iter=1,
@@ -102,9 +128,10 @@ class CustomDQNTrainer(DQNTrainer):
                 _fake_gpus=config["_fake_gpus"],
             )
         replay_op = (
-            Replay(local_buffer=kwargs["local_replay_buffer"])
+            Replay(local_buffer=local_replay_buffer)
             .for_each(lambda x: post_fn(x, workers, config))
             .for_each(train_step_op)
+            .for_each(update_prio)
             .for_each(
                 UpdateTargetNetwork(
                     workers, config["target_network_update_freq"]
